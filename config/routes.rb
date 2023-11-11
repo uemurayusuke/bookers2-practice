d54183b3df7313571928d1b8839689a8f92f6eb2
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-11 00:19:33 +0000

    [Work]4-kadai-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+
+
+  resources :books, only: [:index, :show, :edit]
+
+
+  root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
