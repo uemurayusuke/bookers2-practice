b9c0e88e029058f6d21a61b3ee62334f3b2ac5e6
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-20 04:29:46 +0000

    [Finish]4-kadai3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    is_matching_login_user
     @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
