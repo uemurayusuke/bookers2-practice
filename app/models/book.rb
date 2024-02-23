3b7122454673f3033c365e86a717e69c37379b71
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-02-23 08:53:44 +0000

    コメント/いいね機能復習

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,5 +6,9 @@ class Book < ApplicationRecord
   validates :body, presence: true
   validates :body, length: { maximum: 200 }
 
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+
 
 end
