150e0d62fe7caad9c3ff12b85ff7806a09298760
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-02-23 09:09:30 +0000

    コメント/いいね機能復習

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   get 'home/about', as: 'about'
 
 
-  resources :books, only: [:index, :create, :show, :edit, :destroy, :update]
-  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :destroy, :update] do
+  resource :favorite, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy]
+end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 end
