c2b994753588612592cfa7b77e2e962210ff2bf9
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2024-03-02 12:51:55 +0000

    いいね機能実装中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,7 +9,10 @@ end
 
 
 def destroy
-
+  book = Book.find(params[:book_id])
+  favorite = current_user.favorites.find_by(book_id: book.id)
+  favorite.destroy
+  redirect_to book_path(book)
 end
 
 end
